c25637ad07ea26c871f859280d6a68374ade3e8e
GlickYael <glick.yael3@gmail.com>
2024-11-27 14:41:34 +0200

    types

---
+++ community/src/types/babysitting.type.ts
@@ -0,0 +1,20 @@
+export interface Babysitting{
+    requesterId: string;
+    babysitterId: string;
+    date: Date;
+    location: string;
+    childrenNumber: number;
+    ages: number[];
+    AuthorizedIds: string[];
+}
+
+/*
+Babysitter Request:
+Requester ID
+Babysitter ID
+Date
+Location
+Number of children
+Ages
+Viewing permissions - A list of groups that can view the event
+*/
