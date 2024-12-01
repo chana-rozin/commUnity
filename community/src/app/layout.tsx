03ee8080ea615c6cee2d12412509dfa1e97aa6b3
Tzipora Maroko <maroko@g.jct.ac.il>
2024-12-01 12:55:18 +0200

    navbar added

--- community/src/app/layout.tsx
+++ community/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Noto_Sans_Hebrew } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/Navbar/Navbar";
 
 const notoSansHebrew = Noto_Sans_Hebrew({ 
   subsets: ['hebrew'],
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="he" dir="rtl" className={notoSansHebrew.className}>
-      <body>{children}</body>
+      <body className="flex overflow-hidden flex-col px-12 pt-6 pb-60 bg-violet-50 max-md:px-5 max-md:pb-24">
+        <Navbar/>
+        {children}
+      </body>
     </html>
   );
 }
