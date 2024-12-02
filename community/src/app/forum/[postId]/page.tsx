f08522b6624f092ac119358f3fda18849770c383
chana-rozin <c0556787069@gmail.com>
2024-12-02 12:05:57 +0200

    try

--- community/src/app/forum/[postId]/page.tsx
+++ community/src/app/forum/[postId]/page.tsx
@@ -1,33 +1,46 @@
-"use client"
 import React from 'react';
+import { Metadata } from 'next';
 import OpenPostSection from '@/components/forum/OpenPostSection';
 import { getPostById } from '@/services/posts';
 
-interface PostPageProps {
+type PostPageProps = {
   params: {
     postId: string;
+  };
+};
+
+export async function generateMetadata({ params }: PostPageProps): Promise<Metadata> {
+  try {
+    const post = await getPostById(params.postId);
+    return {
+      title: post.title,
+      description: post.content.slice(0, 160)
+    };
+  } catch (error) {
+    return {
+      title: 'Post Not Found',
+      description: 'The requested post could not be found'
+    };
   }
 }
 
 const PostPage = async ({ params }: PostPageProps) => {
-  
   try {
     const { postId } = params;
-    // Fetch the specific post using the postId from the route
     const post = await getPostById(postId);
 
     return (
       <div>
         <OpenPostSection 
-          _id={post._id}
-          creatorId={post.creatorId}
-          createdDate={post.createdDate}
-          title={post.title}
-          content={post.content}
-          comments={post.comments}
-          likedBy={post.likedBy}
-          communityId="0"
-          images={post.images}
+          _id={post.id} 
+          creatorId={post.creatorId} 
+          createdDate={post.createdDate} 
+          title={post.title} 
+          content={post.content} 
+          comments={post.comments} 
+          likedBy={post.likedBy} 
+          communityId="0" 
+          images={post.images} 
         />
       </div>
     );
