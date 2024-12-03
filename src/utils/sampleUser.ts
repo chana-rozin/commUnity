7f04cfd7010fe22839547f5ed766e6675223e866
chana-rozin <c0556787069@gmail.com>
2024-12-03 14:47:00 +0200

    :)

--- src/utils/sampleUser.ts
+++ src/utils/sampleUser.ts
@@ -2,7 +2,7 @@
 import http from '../services/http'
 import axios from 'axios'
 async function getUser(){
-    const result  =  await axios.get('http://localhost:3000/api/users/674ed9fb04a9dba04cdfaea7');
+    const result  = await http.get("/users/674ed9fb04a9dba04cdfaea7");
     return result.data;
     
 }
