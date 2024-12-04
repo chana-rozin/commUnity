75a84f03f5714ba7af45e3bf42ed3352561bc91a
chana-rozin <c0556787069@gmail.com>
2024-12-04 15:02:48 +0200

    get user's communities rout implementation

---
+++ src/app/api/users/[id]/communities/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { getDocumentByQuery} from "@/services/mongodb";
+import {ObjectId} from 'mongodb'
+
+export async function GET(request: Request) {
+    try {
+        const url = new URL(request.url);
+        const searchQuery = url.searchParams.get('ids')?.split(' ').map(id=>new ObjectId(id));
+        const query = {
+            "_id": { $in: searchQuery }
+        }
+        const communities = await getDocumentByQuery("communities", query);
+        return NextResponse.json(communities)
+    } catch (err) {
+        return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
+    }
+}
