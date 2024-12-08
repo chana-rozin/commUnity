d574a53b395197652d24c8e2445b8b66c463302c
chana-rozin <c0556787069@gmail.com>
2024-12-08 16:01:38 +0200

    checking if there is user or not

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-// src/app/layout.tsx
+"use client";
 import { Noto_Sans_Hebrew } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar/Navbar";
 import QueryProvider from "./QueryProvider";
-
+import useUserStore from "@/stores/userStore";
+import { useRouter } from "next/navigation"; // Import useRouter
+import { useEffect } from "react";
 
 const notoSansHebrew = Noto_Sans_Hebrew({
   subsets: ['hebrew'],
@@ -11,19 +13,30 @@ const notoSansHebrew = Noto_Sans_Hebrew({
   display: 'swap',
 });
 
-export const metadata = {
-  title: 'commUnity',
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  
+  const { user } = useUserStore();
+  const router = useRouter(); // Initialize the router
+
+  // Redirect to /register if no user is found
+useEffect(() => {
+    if (!user) {
+      router.push("/register");
+    }
+  }, [user, router]);
+
   return (
     <html lang="he" dir="rtl" className={notoSansHebrew.className}>
       <body className="flex flex-col px-12 pt-3 pb-60 bg-violet-50 max-md:px-5 max-md:pb-24">
-        <Navbar/>
-        <QueryProvider>
-          {children}
-        </QueryProvider>
+        {
+          user ? (
+            <>
+              <Navbar />
+              <QueryProvider>
+                {children}
+              </QueryProvider>
+            </>
+          ) : null // You can optionally render a loading spinner or nothing while redirecting
+        }
       </body>
     </html>
   );
