e61a13ed9c0821e5e393cfe708d7a52997554bbe
chana-rozin <c0556787069@gmail.com>
2024-12-11 19:43:37 +0200

    starting imp babysitting and add layout for neighborhood-help

---
+++ src/app/neighborhood-help/babysitting/components/RequestCard.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { BabysittingRequest } from '../types';
+import {Babysitting} from '@/types/babysitting.type'
+
+interface RequestCardProps {
+    request: Babysitting;
+}
+
+export function RequestCard({ request }: RequestCardProps) {
+    return (
+        <div className="flex flex-col grow shrink h-60 w-[184px]">
+            <div className="flex flex-col py-5 pr-5 w-full bg-white rounded-2xl">
+                <h2 className="gap-4 self-stretch w-full text-lg font-bold leading-10 text-neutral-950">
+                    {request.requester.name}
+                </h2>
+                <div className="gap-1 self-end mt-1 text-xs leading-4 text-right text-stone-500">
+                    <span className="text-sm font-semibold leading-5 text-stone-500">
+                        תאריך:
+                    </span>
+                    <span className="text-sm leading-4 text-stone-500">
+                        {' '}{request.date.toISOString()}, {request.time.start}-{request.time.end}
+                    </span>
+                    <br />
+                    <span className="text-sm font-semibold leading-5 text-stone-500">
+                        מספר ילדים:
+                    </span>
+                    <span className="text-sm leading-4 text-stone-500">
+                        {' '}{request.childrenNumber}
+                    </span>
+                    <br />
+                    <span className="text-sm font-semibold leading-5 text-stone-500">
+                        גילאים:{' '}
+                    </span>
+                    <span className="text-sm leading-4 text-stone-500">{request.ageRange}</span>
+                    <br />
+                    <span className="text-sm font-semibold leading-5 text-stone-500">
+                        כתובת:
+                    </span>
+                    <span className="text-sm leading-4 text-stone-500">
+                        {' '}{request.requester.address.street} {request.requester.address.houseNumber}
+                    </span>
+                </div>
+                <div className="flex gap-4 self-end mt-1 w-80 max-w-full min-h-[42px]" />
+                <div className="flex gap-3 items-start mt-1 w-full text-base font-medium leading-none text-neutral-100">
+                    <div className="flex gap-5 items-start">
+                        <button
+                            className="flex gap-3 items-center p-2 ml-6 bg-indigo-600 rounded-[50px]"
+                            aria-label="הצע לשמרטף"
+                        >
+                            <span className="self-stretch my-auto">רוצה לשמרטף!</span>
+                            <img
+                                loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/2c3802ac905fb5f7158ac32beb79d55b28a3351cb29b01bbfd7788ea91e194f9?placeholderIfAbsent=true&apiKey=526d563cdba2451a913eb25ca7c41611"
+                                alt=""
+                                className="object-contain shrink-0 self-stretch my-auto aspect-[1.05] w-[21px]"
+                            />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
