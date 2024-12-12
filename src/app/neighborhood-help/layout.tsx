1f60a9d1a01fbb989be8c131c867e4d4ccc5b806
Tzipora Maroko <maroko@g.jct.ac.il>
2024-12-12 13:15:17 +0200

    layout minor change

--- src/app/neighborhood-help/layout.tsx
+++ src/app/neighborhood-help/layout.tsx
@@ -11,16 +11,17 @@ export default function NeighborhoodHelpLayout({ children }: { children: React.R
     const categories = [{ name: "השאלות", href: "/neighborhood-help/loans", isActive: currentCategory === "loans" }, { name: "שמרטפות", href: "/neighborhood-help/babysitting/requests", isActive: currentCategory === "babysitting" }];
 
     return (
-        <div className="flex w-full mt-8 gap-5">
-            <aside className="flex flex-col min-h-[909px] w-[211px] max-w-full" role="complementary">
+        <div className="flex flex-wrap gap-4 items-start mt-5 w-full">
+            <aside className="flex flex-col w-[211px] max-w-full" role="complementary">
                 <ProfileAside categories={categories} />
             </aside>
             {children}
             <div className="flex justify-end flex-1 shrink basis-0 min-w-[260px]">
-                <div className="flex flex-col p-5 w-full max-w-[350px] bg-white rounded-2xl">
+                <div className="flex flex-col p-5 w-full  bg-white rounded-2xl">
                     <EventsNotificationsCard />
                 </div>
             </div>
         </div>
     );
 }
+
