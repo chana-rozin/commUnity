0c94353e74adfc81772f5c03e89b2aff61c372d8
chana-rozin <c0556787069@gmail.com>
2024-12-25 14:03:48 +0200

    layout

--- src/app/neighborhood-help/layout.tsx
+++ src/app/neighborhood-help/layout.tsx
@@ -2,8 +2,7 @@
 import React, { useState } from 'react'
 import { usePathname } from "next/navigation";
 import { ProfileAside } from '@/components/ProfileAside/ProfileAside';
-import { EventsNotificationsCard } from '@/components/EventsNotificationsCard/EventsNotificationsCard';
-import GeneralNotificationsCard from '@/components/GeneralNotificationsCard/GeneralNotificationsCard';
+
 export default function NeighborhoodHelpLayout({ children }: { children: React.ReactNode }) {
 
     const pathname = usePathname();
@@ -16,14 +15,6 @@ export default function NeighborhoodHelpLayout({ children }: { children: React.R
                 <ProfileAside categories={categories} />
             </aside>
             {children}
-            <div className="flex flex-col flex-1 shrink basis-0 min-w-[260px]">
-                <div className="flex flex-col p-5 w-full  bg-white rounded-2xl">
-                    <GeneralNotificationsCard />
-                </div>
-                <div className="flex flex-col p-5 w-full  bg-white rounded-2xl">
-                    <EventsNotificationsCard />
-                </div>
-            </div>
         </div>
     );
 }
