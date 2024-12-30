255f6fc9e7a056916fe269aed5846e64f9cefdd4
Tzipora Maroko <maroko@g.jct.ac.il>
2024-12-30 09:20:02 +0200

    comment input css fixed

--- src/components/animations/Loading.tsx
+++ src/components/animations/Loading.tsx
@@ -16,7 +16,7 @@ const Loading: React.FC<props> = ({height}) => {
             <Puff
                 visible={true}
                 height="80"
-                width="80"
+                width="60"
                 color="#3f51b5"
                 ariaLabel="puff-loading"
             />
